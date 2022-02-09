97bc1befae1ec699420ad7f67a51dd952e552d9b
pozys <pozys@mail.ru>
2022-02-09 12:49:38 +0300

    Добавление кнопок

--- frontend/views/order/success.php
+++ frontend/views/order/success.php
@@ -24,9 +24,12 @@ $vehicle_data = json_decode($vehicle->data)->attributes;
 
     <h3><?= Html::encode($order->dealer->name) ?></h3>
 
-    <p class="lead">
-        <?= Html::encode($vehicle_data->brand), ' ', Html::encode($vehicle_data->model), ' ', Html::encode($vehicle->registration_number) ?>
-    </p>
+    <div class="d-flex flex-row bd-highlight mb-3">
+        <div class="p-2 bd-highlight"><?= Html::encode($vehicle_data->brand) ?>
+            <?= Html::encode($vehicle_data->model) ?></div>
+        <div class="p-2 bd-highlight"><span class="border border-primary rounded"><?= Html::encode($vehicle->registration_number) ?></span></div>
+    </div>
+
 
     <div class="container">
         <div class="row justify-content-md-left">
@@ -61,6 +64,9 @@ $vehicle_data = json_decode($vehicle->data)->attributes;
                 <?= Html::encode(Yii::$app->formatter->asCurrency(($data_attributes->amount - $data_attributes->payment_amount))) ?>
             </div>
         </div>
+        <div class="d-grid gap-2">
+            <button class="btn btn-primary" type="submit">Оплатить</button>
+            <a class="btn btn-outline-primary" href="#" role="button">Подробнее</a>
+        </div>
     </div>
-</div>
 </div>
